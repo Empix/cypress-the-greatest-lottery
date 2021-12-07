f67f883ab4256241b2524980c27c6e557adcd292
Ádrian <adrian.fr.fe@gmail.com>
2021-12-07 16:10:17 -0400

    test: should create a new user

---
+++ cypress/integration/tgl.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+const faker = require('faker');
+
+describe('TGL', () => {
+  const credentials = {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+
+  before(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Should create a new user', () => {
+    cy.get('[href="/register"]').click();
+
+    cy.get('#name').type(credentials.name);
+    cy.get('#email').type(credentials.email);
+    cy.get('#password').type(credentials.password);
+
+    cy.get('button').click();
+    cy.location('href').should('not.contain', '/register');
+  });
+});
